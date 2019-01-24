93f8cf74804e715786cd6df834a33659c6c7ed0b
James Harris <computersherpa@gmail.com>
2019-01-24 17:33:41 -0600

    List groups

--- src/App.js
+++ src/App.js
@@ -1,41 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      groups: []
+    };
     this.getStatus = this.getStatus.bind(this);
     this.getStatus();
   }
   getStatus() {
-    console.log("Started");
     fetch("http://proxy.hkijharris.test/getStatus.php")
       .then(response => response.json())
       .then(json => {
         console.log(json.data);
-        this.setState('systems', json.data);
+        this.setState({groups: json.data});
       })
     ;
   }
   render() {
+    let groups = this.state.groups.map((group, index) => {
+      return <ul key={index} className="group">{group.id}</ul>
+    });
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="monitor">
+        <div className="groups">
+          {groups}
+        </div>
       </div>
     );
   }
