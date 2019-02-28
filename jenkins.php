b0b547f72b7495a4787ffe6d1738adf87705534e
James Harris <computersherpa@gmail.com>
2019-02-28 10:15:48 -0600

    Gitignore secrets file and incorporate into PHP scripts

---
+++ jenkins.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/json");
+
+function fetch($url) {
+	$curl = curl_init($url);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$result = curl_exec($curl);
+	curl_close($curl);
+	// echo $result;
+	return json_decode(trim($result), false);
+}
+
+$jobs = array();
+
+$credentials = $secrets['old-jenkins']['username'] . ':' . $secrets['old-jenkins']['password'];
+
+$oldJenkinsData = fetch("http://$credentials@jenkins.hkipm.com:8080/api/json?tree=jobs[name,description,color,builds[number,result,timestamp]{0,1}]");
+$jobs = array_merge($jobs, $oldJenkinsData->jobs);
+
+$credentials = $secrets['new-jenkins']['username'] . ':' . $secrets['new-jenkins']['password'];
+
+$newJenkinsData = fetch("http://$credentials@jenkins.hkipop.com/api/json?tree=jobs[name,description,color,builds[number,result,timestamp]{0,1}]");
+$jobs = array_merge($jobs, $newJenkinsData->jobs);
+
+echo json_encode($jobs);
---
+++ load-balancer.php
@@ -0,0 +1,42 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-Type: text/json");
+
+// Login to get token
+
+$login_curl = curl_init();
+
+$url = "https://10.1.11.250/restapi/v2/login";
+$credentials = $secrets['load-balancer'];
+
+curl_setopt($login_curl, CURLOPT_POST, 1);
+curl_setopt($login_curl, CURLOPT_POSTFIELDS, $credentials);
+curl_setopt($login_curl, CURLOPT_URL, $url);
+curl_setopt($login_curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($login_curl, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($login_curl, CURLOPT_SSL_VERIFYHOST, 0);
+
+$login_result = curl_exec($login_curl);
+curl_close($login_curl);
+
+$json = json_decode(trim($login_result), true);
+$token = $json['token'];
+$token = base64_encode($token);
+
+// Use login token to get API data
+
+$getStatus_curl = curl_init();
+$url = "https://10.1.11.250/restapi/v2/virtual_service_groups";
+$headers = array(
+	'Authorization: Basic '.$token
+);
+curl_setopt($getStatus_curl, CURLOPT_URL, $url);
+curl_setopt($getStatus_curl, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($getStatus_curl, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($getStatus_curl, CURLOPT_SSL_VERIFYPEER, 0);
+curl_setopt($getStatus_curl, CURLOPT_SSL_VERIFYHOST, 0);
+$getStatus_result = curl_exec($getStatus_curl);
+curl_close($getStatus_curl);
+
+echo $getStatus_result;
