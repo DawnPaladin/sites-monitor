bd48e8ecd7a4a07cc72c7da28d7296300f49c445
James Harris <computersherpa@gmail.com>
2019-03-01 17:53:26 -0600

    Updates

---
+++ precache-manifest.5fd65907f4fcacbe86479bceef87938a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "c66a6d5607a47521d90d",
+    "url": "/static/js/main.c66a6d56.chunk.js"
+  },
+  {
+    "revision": "646cab25e5bd11ecb317",
+    "url": "/static/js/1.646cab25.chunk.js"
+  },
+  {
+    "revision": "c66a6d5607a47521d90d",
+    "url": "/static/css/main.1b9ec59f.chunk.css"
+  },
+  {
+    "revision": "646cab25e5bd11ecb317",
+    "url": "/static/css/1.c90e240d.chunk.css"
+  },
+  {
+    "revision": "09a06688bed684bb27b32df49ee65c74",
+    "url": "/index.html"
+  }
+];
