4a771ae0de603caa78e0459a33b405f050580736
James Harris <computersherpa@gmail.com>
2019-03-01 18:11:55 -0600

    Updates

---
+++ precache-manifest.f828c89beae6d54b0dd66c7e4e282497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8c8fc1255478978dd304",
+    "url": "/sites-monitor/static/js/runtime~main.8c8fc125.js"
+  },
+  {
+    "revision": "7e7747dbee947ce9e364",
+    "url": "/sites-monitor/static/js/main.7e7747db.chunk.js"
+  },
+  {
+    "revision": "646cab25e5bd11ecb317",
+    "url": "/sites-monitor/static/js/1.646cab25.chunk.js"
+  },
+  {
+    "revision": "7e7747dbee947ce9e364",
+    "url": "/sites-monitor/static/css/main.26e49d46.chunk.css"
+  },
+  {
+    "revision": "646cab25e5bd11ecb317",
+    "url": "/sites-monitor/static/css/1.c90e240d.chunk.css"
+  },
+  {
+    "revision": "a1a30499ce74e847b51ec01a2010d170",
+    "url": "/sites-monitor/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5fd65907f4fcacbe86479bceef87938a.js"
+  "/sites-monitor/precache-manifest.f828c89beae6d54b0dd66c7e4e282497.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/sites-monitor/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
